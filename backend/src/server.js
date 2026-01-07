e149d4a90dc5118a18bd24552760451180f22404
zahra a <182934048+zeashel@users.noreply.github.com>
2026-01-08 06:32:57 +0700

    feat(backend): add  /api/products:id endpoint in server.js
    - move temp data as `const products` so its accessible by both /api/products (get all products) and /api/products:id (get 1 product by id)

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,52 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+// temp data, use monggodb later
+const products = [
+    {
+        id: 1,
+        name: "Sepatu",
+        price: 200000,
+        desc: "Sepatu premium berbahan kulit",
+        img: "https://placehold.net/default.svg",
+        discount: null,
+    },
+    {
+        id: 2,
+        name: "Tas",
+        price: 160000,
+        desc: "Tas pesta berbahan kulit",
+        img: "https://placehold.net/default.svg",
+        discount: 50,
+    },
+    {
+        id: 3,
+        name: "Jaket",
+        price: 200000,
+        desc: "Jaket kulit premium berwarna hitam",
+        img: "https://placehold.net/default.svg",
+        discount: 20,
+    }
+]
+
+// endpoint getProducts (fetches all products)
 app.get("/api/products", (req, res) => {
-    // temp data
-    res.json([
-        { id: 1, name: "Mouse", price: 50000 },
-        { id: 2, name: "Keyboard", price: 150000 }
-    ]);
+    res.json(products);
 });
+
+// endpoint getProductsById (fetches 1 product by their id)
+app.get("/api/products/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const product = products.find((p) => p.id === id);
+
+    if (!product) {
+        return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.json(product);
+});
+
 
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
 });
-
-app.use(express.static("src/public"));
