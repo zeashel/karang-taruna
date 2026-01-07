2ea09a355666e84eb6cfc27df73bf689eae9a90a
zahra a <182934048+zeashel@users.noreply.github.com>
2026-01-08 05:20:55 +0700

    feat(frontend): make ProductCards a link to the corresponding ID
    - each card links to `{`/products/${id}`}`, id from argument
    - add new class `card-link` to remove anchor styling to the card (fixes side effect of wrapping it in Link)

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "../scss/product-card.scss";
 
 function ProductCard({ id, name, price, desc, img, discount }) {
@@ -25,19 +26,25 @@ function ProductCard({ id, name, price, desc, img, discount }) {
     }
 
     return (
-        <div className="card product-card h-100" id={id}>
-            <div className="product-image-wrapper">
-                <img src={img} className="card-img-top img-fluid" alt={name} />
-            </div>
+        <Link to={`/products/${id}`} className="card-link">
+            <div className="card product-card h-100" id={id}>
+                <div className="product-image-wrapper">
+                    <img
+                        src={img}
+                        className="card-img-top img-fluid"
+                        alt={name}
+                    />
+                </div>
 
-            <div className="card-body">
-                <h5 className="card-title">{name}</h5>
-                {priceElem}
-                <p className="card-text">
-                    <small className="text-muted">{desc}</small>
-                </p>
+                <div className="card-body">
+                    <h5 className="card-title">{name}</h5>
+                    {priceElem}
+                    <p className="card-text">
+                        <small className="text-muted">{desc}</small>
+                    </p>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 }
 
