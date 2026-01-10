ae566261f8e44bb6eb179fa7e976f46c5281c33c
zahra a <182934048+zeashel@users.noreply.github.com>
2026-01-10 18:29:19 +0700

    feat(frontend): redesign ProductDetail to implement `long_desc` and `details`

--- frontend/src/pages/ProductDetail.jsx
+++ frontend/src/pages/ProductDetail.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getProductById } from "../services/productService";
+import { resolveImageUrl } from "../utils/resolveImageUrl";
 
 export default function ProductDetail() {
     const { id } = useParams();
@@ -12,11 +13,99 @@ export default function ProductDetail() {
 
     if (!product) return <p className="display-5 text-center">Loading...</p>;
 
+    const priceElem =
+        // if discounted
+        product.discount !== null && product.discount !== undefined ? (
+            <>
+                <h2>
+                    <strong>Price: </strong>
+                    <span className="text-primary mb-1">
+                        Rp{product.price * (1 - product.discount / 100)}{" "}
+                    </span>
+                    <small className="text-muted opacity-50 text-decoration-line-through">
+                        Rp{product.price}
+                    </small>
+                    <span class="badge alert-primary" role="alert">
+                        {product.discount}% OFF
+                    </span>
+                </h2>
+            </>
+        ) : (
+            // if no discount
+            <h2>
+                <strong>Price: </strong> Rp{product.price}
+            </h2>
+        );
+
     return (
-        <div className="container py-4">
-            <h2>{product.name}</h2>
-            <p>Harga: Rp {product.price}</p>
-            <p>{product.desc}</p>
+        <div className="container pt-4">
+            <div className="row">
+                <div className="col-md-4">
+                    <img
+                        className="rounded img-fluid"
+                        src={resolveImageUrl(product.img)}
+                    />
+                    <div className="d-flex gap-2 my-3 w-100">
+                        <button className="btn btn-primary btn-lg hover-btn">
+                            Add to Cart
+                        </button>
+                        <button
+                            className="btn btn-outline-primary btn-lg hover-btn"
+                            title="Add to Wishlist"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="currentColor"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="square"
+                                strokeLinejoin="square"
+                                className="lucide lucide-heart-icon lucide-heart"
+                                style={{
+                                    marginBottom: 5,
+                                }}
+                            >
+                                <path d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5" />
+                            </svg>
+                        </button>
+                        <button
+                            className="btn btn-outline-primary btn-lg hover-btn"
+                            title="Give Rating"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="currentColor"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="square"
+                                strokeLinejoin="square"
+                                className="lucide lucide-heart-icon lucide-heart"
+                                style={{
+                                    marginBottom: 5,
+                                }}
+                            >
+                                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+                <div className="col-md-8">
+                    <h4>{product.name}</h4>
+                    <p className="text-muted mb-3">{product.desc}</p>
+                    {priceElem}
+                    <hr />
+                    <h4>Description</h4>
+                    <p>{product.long_desc}</p>
+                    <h4>Details</h4>
+                    <p>{product.details}</p>
+                </div>
+            </div>
         </div>
     );
 }
