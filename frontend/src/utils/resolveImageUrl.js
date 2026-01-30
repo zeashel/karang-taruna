49d64b0f5b0cfacf2266f1ae5bbda4cd4f2accf6
zahra a <182934048+zeashel@users.noreply.github.com>
2026-01-30 19:23:23 +0700

    fix(frontend): disable absolute images in resolveImageUrl
    - external http/https images will not be displayed

--- frontend/src/utils/resolveImageUrl.js
+++ frontend/src/utils/resolveImageUrl.js
@@ -5,9 +5,9 @@ export const resolveImageUrl = (img) => {
     if (!img) return "/img/default.svg";
 
     // already absolute (http / https)
-    if (img.startsWith("http://") || img.startsWith("https://")) {
-        return img;
-    }
+    // if (img.startsWith("http://") || img.startsWith("https://")) {
+    //     return img;
+    // }
 
     // relative path → backend image
     return `${API_URL}${img}`;
