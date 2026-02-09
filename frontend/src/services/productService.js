9ba83eb891b64ab0882e70b2aad8d283eeb269e1
zahra a <182934048+zeashel@users.noreply.github.com>
2026-02-10 04:01:22 +0700

    fix(service): update `PRODUCT_API` in productService

--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -1,6 +1,6 @@
 import { API_URL } from "../config";
 
-const PRODUCT_API = `${API_URL}/api/products`;
+const PRODUCT_API = `${API_URL}/api/tutorials`;
 
 export const getProducts = async () => {
     const res = await fetch(PRODUCT_API);
