ee65a4ae5889594038a3af18dc9c12088e0ae9b7
zahra a <182934048+zeashel@users.noreply.github.com>
2026-02-10 04:08:23 +0700

    fix(frontend): implement new Tutorial object attributes to Products and ProductCard

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -2,34 +2,16 @@ import { Link } from "react-router-dom";
 import "../scss/product-card.scss";
 import { resolveImageUrl } from "../utils/resolveImageUrl";
 
-function ProductCard({ id, name, price, desc, img, discount }) {
-    let priceElem = null;
-    const priceStr = price.toLocaleString("id-ID");
-
-    if (discount !== null && discount !== undefined) {
-        priceElem = (
-            <>
-                <span
-                    className="position-absolute badge rounded-pill bg-primary z-3"
-                    style={{ top: "5px", right: "5px" }}
-                >
-                    {discount}% OFF
-                </span>
-                <p className="card-text text-primary mb-1">
-                    Rp{(price * (1 - discount / 100)).toLocaleString("id-ID")}{" "}
-                    <small className="text-muted opacity-50 text-decoration-line-through">
-                        Rp{priceStr}
-                    </small>
-                </p>
-            </>
-        );
-    } else {
-        priceElem = <p className="card-text mb-1">Rp{priceStr}</p>;
-    }
-
+function ProductCard({ id, name, desc, img, type }) {
     return (
         <Link to={`/products/${id}`} className="card-link">
             <div className="card product-card h-100" id={id}>
+                <span
+                    className="position-absolute badge rounded-pill bg-primary z-3 shadow"
+                    style={{ top: "5px", right: "5px" }}
+                >
+                    {type}
+                </span>
                 <div className="product-image-wrapper">
                     <img
                         src={resolveImageUrl(img)}
@@ -40,7 +22,6 @@ function ProductCard({ id, name, price, desc, img, discount }) {
 
                 <div className="card-body">
                     <h5 className="card-title">{name}</h5>
-                    {priceElem}
                     <p className="card-text">
                         <small className="text-muted">{desc}</small>
                     </p>
--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -5,32 +5,31 @@ import ProductCard from "../components/ProductCard";
 import Loading from "../components/Loading";
 
 function Products() {
-    const [products, setProducts] = useState(null);
+    const [tutorials, setTutorials] = useState(null);
 
     useEffect(() => {
         // useEffect: run this code after React renders
         getProducts()
-            .then((data) => setProducts(data))
+            .then((data) => setTutorials(data))
             .catch((err) => console.error(err));
-        console.log("fetched products from API");
+        console.log("fetched tutorial objects from API");
     }, []);
 
-    if (!products) return <Loading />;
+    if (!tutorials) return <Loading />;
 
     return (
         <div>
-            <h1 className="mb-4">Product List</h1>
+            <h1 className="mb-4">Tutorials</h1>
 
             <div className="product-grid">
-                {products.map((p) => (
+                {tutorials.map((p) => (
                     <ProductCard
                         key={p._id} // react only prop
                         id={p._id}
                         name={p.name}
-                        price={p.price}
                         desc={p.desc}
                         img={p.img}
-                        discount={p.discount}
+                        type={p.type}
                     />
                 ))}
             </div>
